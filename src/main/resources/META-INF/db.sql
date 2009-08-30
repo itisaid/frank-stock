97eaa5b5fb44f0f53638ec3df80c666f2c6f42fa
oldratlee <oldratlee@513dcd0a-8e58-11de-88c3-552db54a6a53>
2009-08-30 03:29:19 +0000

    为创建的对象加上 字符集属性 SQL的关键字改成了 大写

--- src/main/resources/META-INF/db.sql
+++ src/main/resources/META-INF/db.sql
@@ -1,18 +1,18 @@
-drop database if exists stock;
+DROP DATABASE IF EXISTS stock;
 
-create database stock;
+CREATE DATABASE stock CHARACTER SET UTF8;
 
-use stock;
+USE stock;
 
-create table stock_data(
-	id int auto_increment,
-	code char(9),
-	name varchar(64),
-	trade_date date,
-	open_price float(10,2),
-	high_price float(10,2),
-	low_price float(10,2),
-	close_price float(10,2),
-	volume int,
-	primary key(id)
-) DEFAULT CHARSET=utf8;
+CREATE TABLE stock_data(
+	id INT AUTO_INCREMENT,
+	code CHAR(9),
+	name VARCHAR(64),
+	trade_date DATE,
+	open_price FLOAT(10,2),
+	high_price FLOAT(10,2),
+	low_price FLOAT(10,2),
+	close_price FLOAT(10,2),
+	volume INT,
+	PRIMARY KEY(id)
+) DEFAULT CHARSET=UTF8;
